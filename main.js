d79d803c55f6c4f6cf462ded87ce3022af8844cd
Malza0408 <parkjh04080@gmail.com>
2021-10-07 00:31:06 +0900

    JS: Scoll fade

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const navbarMenu = document.querySelector('.navbar__menu');
 const contactBtn = document.querySelector('.home_contact')
 const contactMe = document.querySelector('#contact');
 
+// Scroll Magic 을 이용한 Scrolling
 // scroll 을 인지해서 각 section 에 도달했는지 체크
 spyEls.forEach(function (spyEl) {
   new ScrollMagic
@@ -17,6 +18,16 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
+spyEls.forEach(function (spyEl) {
+  new ScrollMagic
+    .Scene({
+      triggerElement: spyEl,
+      triggerHook: -.3,
+    })
+    .setClassToggle(spyEl, 'foggy')    
+    .addTo(new ScrollMagic.Controller());
+});
+
 // Scroll 시 Navbar 버튼 배경색 생김
 window.addEventListener('scroll',  (evenet) => {
   if (scrollY > 0) {
@@ -25,6 +36,12 @@ window.addEventListener('scroll',  (evenet) => {
   } else {
     navbar.classList.remove('active');
   }
+  // Scroll 시 각 section 투명하게
+  const foggy = document.querySelectorAll('.foggy');
+  const lastEl = foggy[foggy.length - 1];
+  const elHeight = lastEl.getBoundingClientRect().height;
+  const y = lastEl.getBoundingClientRect().y;
+  lastEl.style.opacity = 1 - (Math.abs(y) / elHeight);
 });
 
 // navbar_menu click 시 해당 section으로 scroll
