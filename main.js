3406ee758bc35a7a06d8071f1f7eedd5d4ebeae0
Malza0408 <parkjh04080@gmail.com>
2021-10-06 23:07:37 +0900

    JS: Contactme Btn

--- main.js
+++ main.js
@@ -3,8 +3,10 @@
 const spyEls = document.querySelectorAll('section.scroll-spy');
 const navbar = document.querySelector('#navbar');
 const navbarMenu = document.querySelector('.navbar__menu');
-const contactBtn = document.querySelector('home_contact')
+const contactBtn = document.querySelector('.home_contact')
+const contactMe = document.querySelector('#contact');
 
+// scroll 을 인지해서 각 section 에 도달했는지 체크
 spyEls.forEach(function (spyEl) {
   new ScrollMagic
     .Scene({
@@ -15,6 +17,7 @@ spyEls.forEach(function (spyEl) {
     .addTo(new ScrollMagic.Controller());
 });
 
+// Scroll 시 Navbar 버튼 배경색 생김
 window.addEventListener('scroll',  (evenet) => {
   if (scrollY > 0) {
     navbar.classList.add('active');
@@ -24,10 +27,16 @@ window.addEventListener('scroll',  (evenet) => {
   }
 });
 
+// navbar_menu click 시 해당 section으로 scroll
 navbarMenu.addEventListener('click', (event) => {
   if (event.target.dataset.selected === undefined) return;
   const target = event.target;
   const seleted = target.dataset.selected;
   const menu = document.querySelector(seleted);
   menu.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+})
+
+// contactMe 버튼 click 시 contactMe 로 scroll
+contactBtn.addEventListener('click', (event) => {
+  contactMe.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
 })
