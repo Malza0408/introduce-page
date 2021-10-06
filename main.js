0483cc4631bb9009e3be2bcd00a3b9302afbbce8
Malza0408 <parkjh04080@gmail.com>
2021-10-06 22:52:50 +0900

    JS: Scroll menu

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+'use strict';
+
 const spyEls = document.querySelectorAll('section.scroll-spy');
+const navbar = document.querySelector('#navbar');
+const navbarMenu = document.querySelector('.navbar__menu');
+const contactBtn = document.querySelector('home_contact')
 
 spyEls.forEach(function (spyEl) {
   new ScrollMagic
@@ -9,3 +14,20 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show')    
     .addTo(new ScrollMagic.Controller());
 });
+
+window.addEventListener('scroll',  (evenet) => {
+  if (scrollY > 0) {
+    navbar.classList.add('active');
+    
+  } else {
+    navbar.classList.remove('active');
+  }
+});
+
+navbarMenu.addEventListener('click', (event) => {
+  if (event.target.dataset.selected === undefined) return;
+  const target = event.target;
+  const seleted = target.dataset.selected;
+  const menu = document.querySelector(seleted);
+  menu.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+})
